076e43051f712b3ed8c1c9e0596e681ff9abaf69
csdn <qinfan@csdn.net>
2020-01-17 14:58:39 +0800

    一些脚本

--- 7_求字符串每个字符的次数.py
+++ 7_求字符串每个字符的次数.py
@@ -1,9 +1,17 @@
-a = 'asfhiuafjbiuvsjbauidhuivbaiuhsvofjwqfzmnvchaiuhudvnqiuvhsnfwiuhvnaljfie'
+str1 = 'asfhiuafjbiuvsjbauidhuivbaiuhsvofjwqfzmnvchaiuhudvnqiuvhsnfwiuhvnaljfie'
 
-count = dict()
-for i in a:
-    if i in count:
-        count[i] += 1
+str_count_dict = dict()
+for i in str1:
+    if i in str_count_dict:
+        str_count_dict[i] += 1
     else:
-        count[i] = 1
-print(count)
+        str_count_dict[i] = 1
+print(str_count_dict)
+
+# 按出现次数，逆序排放
+sort_count = sorted(str_count_dict, key=lambda x: str_count_dict[x], reverse=True)
+print(sort_count)
+
+# 输出出现次数最多的
+max_count = max(str_count_dict, key=lambda x: str_count_dict[x])
+print(max_count)
